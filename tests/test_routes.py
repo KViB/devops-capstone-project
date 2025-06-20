a03397a41e1e39c817d4870105d2f4b9889e0590
KViB <karis.bagde20@gmail.com>
2025-06-20 16:54:20 +0530

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -13,6 +13,8 @@ from service.common import status  # HTTP Status Codes
 from service.models import db, Account, init_db
 from service.routes import app
 
+from service import talisman
+
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
 )
@@ -35,7 +37,8 @@ class TestAccountService(TestCase):
         app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
         app.logger.setLevel(logging.CRITICAL)
         init_db(app)
-
+        talisman.force_https = False
+  
     @classmethod
     def tearDownClass(cls):
         """Runs once before test suite"""
